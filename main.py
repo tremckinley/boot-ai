0f5627b6b896a5be368a702b89005faabc5c10cd
tremckinley <tremckinley901@gmail.com>
2025-06-19 20:27:02 -0500

    Refactor argument handling to improve prompt validation and verbosity output

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
-contents = sys.argv[1]
-if (not contents):
+args = sys.argv[1:]
+
+#print(args)
+
+if (len(args)< 1):
     print("You need a prompt!")
     exit(1)
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=contents
+    model='gemini-2.0-flash-001', contents=args[0]
 )
 
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+if ("--verbose" in args):
+    #print(response.usage_metadata)
+    print("User prompt:", args[0])
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+
 print("Response:")
 print(response.text)
